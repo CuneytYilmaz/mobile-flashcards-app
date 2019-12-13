9f089a2027d1bf185c416c14aabf730ce145c2dc
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-13 21:19:26 +0300

    Folder structure created, reducer, action and getDecks function created

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import DeckList from './components/DeckList'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={ createStore(reducer) }>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app</Text>
+          <DeckList />
+        </View>
+    </Provider>
+    
   );
 }
 
